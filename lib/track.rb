d7aab244caf01e2defc56e5090007957a86ddb0b
Carlos Atkinson Ferreira Filho <carlos.atks@gmail.com>
2016-11-16 22:14:49 -0200

    Track Lib and Specs
    	Added Track functionality and specs.

--- lib/track.rb
+++ lib/track.rb
@@ -1,57 +1,108 @@
 # coding: utf-8
 class Track
-  attr_accessor :number, :talks
-  attr_reader :morning_talks,:afternoon_talks
+  START_TIME = '9:00'.freeze
+  LUNCH_TIME = '12:00'.freeze
+  AFTERNOON_START_TIME = '13:00'.freeze
+  MAX_END_TIME = '17:00'.freeze
+
+  attr_accessor :number
 
   def initialize(number,*talks)
     self.number = number
-    self.talks = talks
+    talks.each do |talk|
+      add_talk talk
+    end
   end
 
   def add_talk(talk)
-    @talks << talk
+    talk.start_time = first_time_avaible_in_morning
+    if can_be_added_in_morning?(talk)
+      @morning_talks ||= []
+      @morning_talks << talk
+      return true
+    end
+
+    talk.start_time = first_time_avaible_in_afternoon
+    if can_be_added_in_afternoon?(talk)
+      @afternoon_talks ||= []
+      @afternoon_talks << talk
+      return true
+    end
   end
 
   def name
     "Track #{number}"
   end
 
-  def organize_talks(begining_time)
-    all_talks = order_by_duration!.dup
-    time = begining_time
-    number_of_talks = 0
-
-    #morning talks
-    while time.hour < 12 && number_of_talks < all_talks.size
-      time = begining_time
-      time += all_talks.first(number_of_talks).collect{ |talk| talk.duration_in_minutes.to_i}.reduce(:+).to_i * 60
-      number_of_talks += 1
-    end
-
-    @morning_talks = set_time(all_talks.shift(number_of_talks), begining_time)
-    #afternoon talks
-    begining_time = Time.new(time.year,time.month,time.day, '13:00')
-    time = begining_time
-    number_of_talks = 0
-
-    while time.hour < 17 && number_of_talks < all_talks.size
-      time = begining_time
-      time += all_talks.first(number_of_talks).collect{ |talk| talk.duration_in_minutes.to_i}.reduce(:+).to_i * 60
-      number_of_talks += 1
-    end
-
-    @afternoon_talks = set_time(all_talks.shift(number_of_talks), begining_time)
-
+  def talks
+    @talks = @morning_talks.to_a + @afternoon_talks.to_a
   end
-  def order_by_duration!(direction = 'asc')
-    if direction == 'desc'
-      order_by_duration!.reverse
+
+  def morning_talks
+    @morning_talks || []
+  end
+
+  def afternoon_talks
+    @afternoon_talks || []
+  end
+
+
+  def can_be_added_in_morning?(talk)
+    return true if @morning_talks.nil?
+    talk.time_duration <= remaning_time_in_morning
+  end
+
+  def remaning_time_in_morning
+    last_time = @morning_talks ? @morning_talks.last.end_time : Track.beginning_time
+    (Track.lunch_time - last_time) / 60 #transform in minutes
+  end
+
+  def can_be_added_in_afternoon?(talk)
+    return true if @afternoon_talks.nil?
+    talk.time_duration <= remaning_time_in_afternoon
+  end
+
+  def remaning_time_in_afternoon
+    last_time = @afternoon_talks ? @afternoon_talks.last.end_time : Track.afternoon_begin_time
+    (Track.max_end_time - last_time) / 60 #transform in minutes
+  end
+
+
+  def self.beginning_time
+    t = Time.now
+    Time.new(t.year, t.month, t.day, START_TIME)
+  end
+
+  def self.afternoon_begin_time
+    t = Time.now
+    Time.new(t.year, t.month, t.day, AFTERNOON_START_TIME)
+  end
+
+  def self.lunch_time
+    t = Time.now
+    Time.new(t.year, t.month, t.day, LUNCH_TIME)
+  end
+
+  def self.max_end_time
+    t = Time.now
+    Time.new(t.year, t.month, t.day, MAX_END_TIME)
+  end
+
+  def first_time_avaible_in_morning
+    if morning_talks.empty?
+      Track.beginning_time
     else
-      self.talks.sort_by!{ |talk| talk.duration_in_minutes.to_i }
+      @morning_talks.last.end_time
     end
   end
 
-  private
+  def first_time_avaible_in_afternoon
+    if afternoon_talks.empty?
+      Track.afternoon_begin_time
+    else
+      afternoon_talks.last.end_time
+    end
+  end
 
   def set_time(my_talks,start_time)
     time = start_time
--- spec/track_spec.rb
+++ spec/track_spec.rb
@@ -7,6 +7,13 @@ describe Track do
     track_instance.add_talk(Talk.new("Awesome Talk 10min"))
     track_instance
   end
+
+  let(:track_instance_with_full_morning) do
+    track_instance.add_talk(Talk.new("Okay Talk 60min"))
+    track_instance.add_talk(Talk.new("Sad Talk 60min"))
+    track_instance.add_talk(Talk.new("Bum Talk 60min"))
+    track_instance
+  end
   describe 'attributes' do
     describe 'number' do
       it "can be created with number" do
@@ -21,67 +28,154 @@ describe Track do
     end
   end
 
+  describe "::beginning_time" do
+    it "returns a time starting at 9:00am" do
+      expect(Track.beginning_time.hour).to eq(9)
+      expect(Track.beginning_time.min).to eq(0)
+    end
+  end
+
+  describe "::lunch_time" do
+    it "returns a time starting at 12:00am" do
+      expect(Track.lunch_time.hour).to eq(12)
+      expect(Track.lunch_time.min).to eq(0)
+    end
+  end
+
+  describe "::afternoon_begin_time" do
+    it "returns a time starting at 13:00am" do
+      expect(Track.afternoon_begin_time.hour).to eq(13)
+      expect(Track.afternoon_begin_time.min).to eq(0)
+    end
+  end
+
+  describe "::max_end_time" do
+    it "returns a time of 17:00am" do
+      expect(Track.max_end_time.hour).to eq(17)
+      expect(Track.max_end_time.min).to eq(0)
+    end
+  end
+
+  describe "#remaning_time_in_morning" do
+    context "when no talk was added" do
+      it "returns the 3 hours" do
+        expect(track_instance.remaning_time_in_morning).to eq(3 * 60) # 3 hours in minutes
+      end
+    end
+
+    context "when a talk of 45 minutes is added" do
+      before(:each){ track_instance.add_talk(Talk.new("Some cool talk 45min"))}
+      it "returns the 2 hours and 15 minutes" do
+        expect(track_instance.remaning_time_in_morning).to eq((2 + 15.0 / 60) * 60) # 2 hours and 5 minutes in minutes
+      end
+    end
+
+    context "when morning talks are full" do
+      before(:each){ track_instance.add_talk(Talk.new("Some cool talk 180min"))}
+      it "returns 0 minutes" do
+        expect(track_instance.remaning_time_in_morning).to eq(0)
+      end
+    end
+  end
+
+  describe "#remaning_time_in_afternoon" do
+    context "when no talk was added" do
+      it "returns the 4 hours" do
+        expect(track_instance.remaning_time_in_afternoon).to eq(4 * 60) # 4 hours in minutes
+      end
+    end
+
+    context "when a talk of 45 minutes is added" do
+      before(:each){ track_instance_with_full_morning.add_talk(Talk.new("Some cool talk 45min"))}
+      it "returns the 3 hours and 15 minutes" do
+        expect(track_instance_with_full_morning.remaning_time_in_afternoon).to eq((3 + 15.0 / 60) * 60) # 3 hours and 5 minutes in minutes
+      end
+    end
+
+    context "when afternoon talks are full" do
+      before(:each){ track_instance_with_full_morning.add_talk(Talk.new("Some cool talk 240min"))}
+      it "returns 0 minutes" do
+        expect(track_instance.remaning_time_in_afternoon).to eq(0)
+      end
+    end
+  end
+
+  describe '#can_be_added_in_morning?' do
+    context "when a talk of 170 minutes is added" do
+      before(:each){ track_instance.add_talk(Talk.new("Some cool talk 170min"))}
+      it "can add a talk of 10min" do
+        expect(track_instance.can_be_added_in_morning?(Talk.new("Fast Talk 10min"))).to be true
+      end
+      it "can't add a talk of 11min" do
+        expect(track_instance.can_be_added_in_morning?(Talk.new("Fast Talk 11min"))).to be false
+      end
+    end
+  end
+
+  describe '#can_be_added_in_afternoon?' do
+    context "when a talk of 230 minutes is added" do
+      before(:each){ track_instance_with_full_morning.add_talk(Talk.new("Some cool talk 230min"))}
+      it "can add a talk of 10min" do
+        expect(track_instance_with_full_morning.can_be_added_in_afternoon?(Talk.new("Fast Talk 10min"))).to be true
+      end
+      it "can't add a talk of 11min" do
+        expect(track_instance_with_full_morning.can_be_added_in_afternoon?(Talk.new("Fast Talk 11min"))).to be false
+        puts track_instance_with_full_morning.morning_talks.inspect
+      end
+    end
+  end
+
+  describe '#first_time_avaible_in_morning' do
+    context 'when no talk is added' do
+      it "returns 9:00" do
+        expect(track_instance.first_time_avaible_in_morning.hour).to eq(9)
+        expect(track_instance.first_time_avaible_in_morning.min).to eq(0)
+      end
+    end
+    context 'when a talk of 15 minutes is added' do
+      before(:each){track_instance.add_talk(Talk.new('Zzzzz 15min'))}
+      it "returns 9:15" do
+        expect(track_instance.first_time_avaible_in_morning.hour).to eq(9)
+        expect(track_instance.first_time_avaible_in_morning.min).to eq(15)
+      end
+    end
+  end
+
+  describe '#first_time_avaible_in_afternoon' do
+    context 'when no talk is added in afternoon' do
+      it "returns 13:00" do
+        expect(track_instance_with_full_morning.first_time_avaible_in_afternoon.hour).to eq(13)
+        expect(track_instance_with_full_morning.first_time_avaible_in_afternoon.min).to eq(0)
+      end
+    end
+    context 'when a talk of 15 minutes is added' do
+      before(:each){track_instance_with_full_morning.add_talk(Talk.new('Zzzzz 15min'))}
+      it "returns 13:15" do
+        expect(track_instance_with_full_morning.first_time_avaible_in_afternoon.hour).to eq(13)
+        expect(track_instance_with_full_morning.first_time_avaible_in_afternoon.min).to eq(15)
+      end
+    end
+  end
+
   describe '#add_new_talk' do
-    it "add a new talk" do
-      expect{track_instance.add_talk(Talk.new('Some Talk 12min'))}
-        .to change{track_instance.talks.size}.by(1)
-    end
-  end
-
-  describe '#talks' do
-    context 'when talks are added' do
-      it "returns all the talks" do
-        expect(track_instance_with_talks.talks).to all(be_an(Talk))
+    context 'with a empty track' do
+      it "add a new talk" do
+        expect{track_instance.add_talk(Talk.new('Some Talk 12min'))}
+          .to change{track_instance.talks.size}.by(1)
       end
-      it "is not empty" do
-        expect(track_instance_with_talks.talks).to_not be_empty
-      end
-    end
-  end
 
-  describe '#name' do
-    it "returns 'Track {id}'" do
-      expect(track_instance.name).to eq "Track #{track_instance.number}"
-    end
-  end
-
-  describe '#order_by_duration!' do
-    it "order by the duration" do
-      expect(track_instance_with_talks.order_by_duration!).to eq track_instance_with_talks.talks.sort_by{ |talk| talk.duration_in_minutes.to_i }
-    end
-    context 'with "desc"' do
-      it "order by the duration fot the bigger to the small" do
-        expect(track_instance_with_talks.order_by_duration!("desc")).to eq track_instance_with_talks.talks.sort_by{ |talk| talk.duration_in_minutes.to_i }.reverse
+      it "add to morning_talks" do
+        expect{track_instance.add_talk(Talk.new('Some Talk 12min'))}
+          .to change{track_instance.morning_talks.size}
       end
     end
 
-    context 'with asc' do
-      it "order by the duration for the small to the bigger" do
-        expect(track_instance_with_talks.order_by_duration!("asc")).to eq track_instance_with_talks.talks.sort_by{ |talk| talk.duration_in_minutes.to_i }
+    context 'with a track with full morning talks' do
+      it "add to afternoon talks" do
+        expect{track_instance_with_full_morning.add_talk(Talk.new('Some Talk 12min'))}
+          .to change{track_instance_with_full_morning.afternoon_talks.size}
       end
     end
   end
 
-  describe 'morning_talks' do
-    before(:each){track_instance_with_talks.organize_talks(Time.new(2016,12,1,'9:00'))}
-    it "returns the morning talks" do
-      expect(track_instance_with_talks.morning_talks).to all(be_an(Talk))
-    end
-
-    it "not empty" do
-      expect(track_instance_with_talks.morning_talks).to_not be_empty
-    end
-
-    it "begins at 9am" do
-      expect(track_instance_with_talks.morning_talks.first.start_time.hour).to eq(9)
-    end
-
-    it "don't pass 12" do
-      expect(track_instance_with_talks.morning_talks.last.end_time.hour)
-        .to be <= 12
-      if track_instance_with_talks.morning_talks.last.end_time.hour == 12
-        expect(track_instance_with_talks.morning_talks.last.min).to eq(0)
-      end
-    end
-  end
 end
